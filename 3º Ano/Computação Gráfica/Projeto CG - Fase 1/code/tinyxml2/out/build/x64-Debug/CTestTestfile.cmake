f3d2cdbd17c15c60a572d40f1c9c826a9e77ddea
Goulart98 <65976315+Goulart98@users.noreply.github.com>
2023-09-04 18:53:13 +0100

    Delete 3º Ano/Computação Gráfica/Projeto CG - Fase 1/code directory

--- 3º Ano/Computação Gráfica/Projeto CG - Fase 1/code/tinyxml2/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2
-# Build directory: C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2/out/build/x64-Debug/xmltest.exe")
-set_tests_properties(xmltest PROPERTIES  WORKING_DIRECTORY "C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2/out/build/x64-Debug" _BACKTRACE_TRIPLES "C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2/CMakeLists.txt;104;add_test;C:/Users/Pedro Martins/Documents/CG/fase1/code/tinyxml2/CMakeLists.txt;0;")
